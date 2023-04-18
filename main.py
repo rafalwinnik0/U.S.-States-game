a94acc97a49ba04a415fed7d7fb886748b01aca5
rafalwinnik0 <130064427+rafalwinnik0@users.noreply.github.com>
2023-04-18 22:33:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+turtle.setup(725, 491)
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+turtle.exitonclick()
