91a308b888bc4d8b1e0295ff24a3010ce9688402
rafalwinnik0 <130064427+rafalwinnik0@users.noreply.github.com>
2023-04-20 19:27:59 +0200

    finished

--- main.py
+++ main.py
@@ -8,20 +8,24 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 data = pandas.read_csv("50_states.csv")
+guessed_states = []
 
-while True:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?")
-    answer_state in data.state.unique()
-
-    finded_state = data[data.state == answer_state]
-    state = turtle.Turtle()
-    state.hideturtle()
-    state.penup()
-    state.goto(finded_state.x.item(), finded_state.y.item())
-    state.write(finded_state.state.item())
-
-#
-
-turtle.exitonclick()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"Guess the State {len(guessed_states)}/50",
+                                    prompt="What's another state name?")
+    if answer_state in guessed_states:
+        print(f"You repeated a state: {answer_state}")
+    elif answer_state in data.state.unique():
+        finded_state = data[data.state == answer_state]
+        state = turtle.Turtle()
+        state.hideturtle()
+        state.penup()
+        state.goto(finded_state.x.item(), finded_state.y.item())
+        state.write(finded_state.state.item())
+        guessed_states.append(answer_state)
+    elif answer_state == "exit":
+        break
+    else:
+        print(f"There is no state like {answer_state} in U.S.")
 
 
